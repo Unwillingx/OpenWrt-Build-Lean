c02916d3bdfec127b260ec90979dac0fa0b895c2
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-06-08 14:37:43 +0800

    Add files via upload

---
+++ clash/modify2.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+import yaml
+import os
+with open('./config/kelecloud.yaml', 'rb') as f:
+    x = yaml.safe_load(f)
+Proxy = ['HK', 'SGP', 'JP', 'TW', 'USA', 'VN']
+HK = []
+SGP = []
+JP = []
+TW = []
+VN = []
+USA = []
+for p in x['proxies']:
+    name = p['name']
+    if '香' in name:
+        Proxy.append(name)
+        HK.append(name)
+    elif '日' in name:
+        Proxy.append(name)
+        JP.append(name)
+    elif '城' in name:
+        Proxy.append(name)
+        SGP.append(name)
+    elif '台' in name:
+        Proxy.append(name)
+        TW.append(name)
+    elif '美' in name:
+        Proxy.append(name)
+        USA.append(name)
+    elif '南' in name:
+        Proxy.append(name)
+        VN.append(name)
+Google = Proxy[6:]
+Disneyplus = Google
+Instagram = ['HK', 'SGP', 'JP', 'TW', 'USA', 'VN']
+Youtube = ['HK', 'SGP', 'JP', 'TW', 'USA', 'VN']
+Spotify = ['HK', 'SGP', 'JP', 'TW', 'USA', 'VN', 'DIRECT']
+Github = ['HK', 'SGP', 'JP', 'TW', 'USA', 'VN']
+Twitter = ['HK', 'SGP', 'JP', 'TW', 'USA', 'VN']
+Telegram = ['HK', 'SGP', 'JP', 'TW', 'USA', 'VN']
+Microsoft = ['HK', 'SGP', 'JP', 'TW', 'USA', 'VN', 'DIRECT']
+
+pgs = []
+pgs.append({'name':'Proxy', 'type':'select', 'proxies':Proxy})
+pgs.append({'name':'Google', 'type':'select', 'proxies':Google})
+pgs.append({'name':'Disneyplus', 'type':'select', 'proxies':Disneyplus})
+pgs.append({'name':'Instagram', 'type':'select', 'proxies':Instagram})
+pgs.append({'name':'Youtube', 'type':'select', 'proxies':Youtube})
+pgs.append({'name':'Spotify', 'type':'select', 'proxies':Spotify})
+pgs.append({'name':'Github', 'type':'select', 'proxies':Github})
+pgs.append({'name':'Twitter', 'type':'select', 'proxies':Twitter})
+pgs.append({'name':'Telegram', 'type':'select', 'proxies':Telegram})
+pgs.append({'name':'Microsoft', 'type':'select', 'proxies':Microsoft})
+pgs.append({'name':'HK', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
+            'proxies':HK, 'url': 'http://www.gstatic.com/generate_204', 'interval': '60'})
+pgs.append({'name':'SGP', 'type': 'url-test', 'proxies':SGP,
+            'url': 'http://www.gstatic.com/generate_204', 'interval': '60'})
+pgs.append({'name':'TW', 'type': 'url-test', 'proxies':TW,
+            'url': 'http://www.gstatic.com/generate_204', 'interval': '60'})
+pgs.append({'name':'JP', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
+            'proxies':JP, 'url': 'http://www.gstatic.com/generate_204', 'interval': '60'})
+pgs.append({'name':'USA', 'type': 'url-test', 'proxies':USA,
+            'url': 'http://www.gstatic.com/generate_204', 'interval': '60'})
+pgs.append({'name':'VN', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
+            'proxies':VN, 'url': 'http://www.gstatic.com/generate_204', 'interval': '60'})
+rps = {}
+rps['Google'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Google.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Google.yaml'}
+rps['Youtube'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Youtube.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/YouTube.yaml'}
+rps['Disneyplus'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Disneyplus.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/DisneyPlus.yaml'}
+rps['Instagram'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Instagram.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Instagram.yaml'}
+
+rps['Facebook'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Facebook.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Facebook.yaml'}
+
+rps['Spotify'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Spotify.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Spotify.yaml'}
+
+rps['Github'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Github.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Github.yaml'}
+
+rps['Twitter'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Twitter.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Twitter.yaml'}
+rps['Telegram'] = {'type': 'http', 'behavior': 'ipcidr', 'path':'./rule_provider/Telegram.yaml',
+                           'url':'https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/telegramcidr.txt'}
+rps['Microsoft'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Microsoft.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Microsoft.yaml'}
+rps['ProxyGFW'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/ProxyGFW.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/ProxyGFWlist.yaml'}
+rs = []
+rs.append('RULE-SET,Telegram,Telegram')
+rs.append('RULE-SET,Twitter,Twitter')
+rs.append('RULE-SET,Instagram,Instagram')
+rs.append('RULE-SET,Facebook,Instagram')
+rs.append('RULE-SET,Youtube,Youtube')
+rs.append('RULE-SET,Google,Google')
+rs.append('RULE-SET,Spotify,Spotify')
+rs.append('RULE-SET,Github,Github')
+rs.append('RULE-SET,Microsoft,Microsoft')
+rs.append('RULE-SET,ProxyGFW,Proxy')
+rs.append('GEOIP,CN,DIRECT')
+rs.append('MATCH,Proxy')
+z = {}
+for k in x.keys():
+    if k == 'proxy-groups':
+        z[k] = pgs
+    elif k == 'rules':
+        z[k] = rs 
+    else:    
+        z[k] = x[k]
+z['rule-providers'] = rps
+with open('./config/myconfig.yaml', 'w', encoding='utf-8') as file:
+    file.write(yaml.dump(z, allow_unicode=True))
+os.system('service openclash restart')
+
+
+
