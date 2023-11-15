3ad8f9ee2a85cc803bcbd48d3d87d93b1bdf7c3b
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-11-15 18:32:02 +0800

    Delete mosdns directory

--- mosdns/mosdns_update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./etc/mosdns/rule/ruleupdate.sh
-service mosdns stop
-mosdns start -c /etc/mosdns/config_custom.yaml
--- mosdns/ruleupdate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget -cO china_ip_list.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/cn.txt
-wget -cO direct_list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt
-wget -cO proxy_list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt
-wget -cO reject_list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt
