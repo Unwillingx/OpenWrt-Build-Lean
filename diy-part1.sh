6b7aa2bc71acd6701635c1057e280cc481976f5c
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-11-18 14:51:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
+grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
