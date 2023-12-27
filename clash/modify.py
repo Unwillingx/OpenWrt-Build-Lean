071071f611cbce9c31d6a315986bd7d851ae248f
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-12-27 16:51:26 +0800

    Update modify.py

--- clash/modify.py
+++ clash/modify.py
@@ -41,6 +41,7 @@ JP = []
 TW = []
 VN = []
 USA = []
+testtime='60'
 for p in x['proxies']:
     name = p['name']
     if '香' in name:
@@ -86,17 +87,17 @@ pgs.append({'name':'Twitter', 'type':'select', 'proxies':Twitter})
 pgs.append({'name':'Telegram', 'type':'select', 'proxies':Telegram})
 pgs.append({'name':'Microsoft', 'type':'select', 'proxies':Microsoft})
 pgs.append({'name':'HK', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
-            'proxies':HK, 'url': 'http://www.gstatic.com/generate_204', 'interval': '300'})
+            'proxies':HK, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
 pgs.append({'name':'SGP', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
-            'proxies':SGP, 'url': 'http://www.gstatic.com/generate_204', 'interval': '300'})
-pgs.append({'name':'TW', 'type': 'url-test', 'proxies':TW,
-            'url': 'http://www.gstatic.com/generate_204', 'interval': '300'})
+            'proxies':SGP, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
+pgs.append({'name':'TW', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
+            'proxies':TW, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
 pgs.append({'name':'JP', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
-            'proxies':JP, 'url': 'http://www.gstatic.com/generate_204', 'interval': '300'})
-pgs.append({'name':'USA', 'type': 'url-test', 'proxies':USA,
-            'url': 'http://www.gstatic.com/generate_204', 'interval': '300'})
+            'proxies':JP, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
+pgs.append({'name':'USA', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
+            'proxies':USA, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
 pgs.append({'name':'VN', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
-            'proxies':VN, 'url': 'http://www.gstatic.com/generate_204', 'interval': '300'})
+            'proxies':VN, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
 rps = {}
 rps['Google'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Google.yaml',
                            'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Google.yaml'}
