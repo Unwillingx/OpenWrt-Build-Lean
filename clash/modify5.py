c21eafc2968ca7468ee03645156576f1c240fe90
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-03-21 22:29:32 +0800

    Update modify5.py

--- clash/modify5.py
+++ clash/modify5.py
@@ -129,4 +129,4 @@ z['rule-providers'] = rps
 os.system('rm ./config/myconfig5.yaml')
 with open('./config/myconfig5.yaml', 'w') as file:
     file.write(yaml.dump(z, allow_unicode=True))
-os.system('/etc/init.d/openclash restart')
+# os.system('/etc/init.d/openclash restart')
